6a7f93b41543f4e3db8d1ff4eb93fd44985d23c4
oku3san <takaya0303@gmail.com>
2023-07-31 20:59:40 +0900

    Add ecs settings

---
+++ terraform/ecs.tf
@@ -0,0 +1,70 @@
+resource "aws_iam_role" "ecs-task-execution-role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version": "2008-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+              "Service": "ecs-tasks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+}
+
+data "aws_iam_policy" "AmazonECSTaskExecutionRolePolicy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role" {
+  role       = aws_iam_role.ecs-task-execution-role.name
+  policy_arn = data.aws_iam_policy.AmazonECSTaskExecutionRolePolicy.arn
+}
+
+
+resource "aws_iam_role" "ecs-task-role" {
+  name = "ecs-task-role"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version": "2008-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+              "Service": "ecs-tasks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_policy" "ecs-task-role" {
+  name   = "ecs-task-role-policy"
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+              "*"
+          ],
+          "Resource": "*"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-role" {
+  role       = aws_iam_role.ecs-task-role.name
+  policy_arn = aws_iam_policy.ecs-task-role.arn
+}
