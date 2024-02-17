acb3d99a2c85cc2ca7770614d5b96e4dae8f2dd7
oku3san <takaya0303@gmail.com>
2024-02-18 00:49:49 +0900

    Update

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,36 +1,36 @@
-#locals {
-#  repository_name = [
-#    "sveltekit",
-#    "nginx"
-#  ]
-#}
-#
-#resource "aws_ecr_repository" "main" {
-#  for_each = toset(local.repository_name)
-#
-#  name                 = each.value
-#}
-#
-#resource "aws_ecr_lifecycle_policy" "main" {
-#  for_each = aws_ecr_repository.main
-#
-#  policy = jsonencode(
-#    {
-#      rules = [
-#        {
-#          action = {
-#            type = "expire"
-#          }
-#          description  = "最新の2つを残してイメージを削除する"
-#          rulePriority = 1
-#          selection = {
-#            countNumber = 2
-#            countType   = "imageCountMoreThan"
-#            tagStatus   = "any"
-#          }
-#        },
-#      ]
-#    }
-#  )
-#  repository = each.value.name
-#}
+locals {
+  repository_name = [
+    "sveltekit",
+    "nginx"
+  ]
+}
+
+resource "aws_ecr_repository" "main" {
+  for_each = toset(local.repository_name)
+
+  name                 = each.value
+}
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  for_each = aws_ecr_repository.main
+
+  policy = jsonencode(
+    {
+      rules = [
+        {
+          action = {
+            type = "expire"
+          }
+          description  = "最新の2つを残してイメージを削除する"
+          rulePriority = 1
+          selection = {
+            countNumber = 2
+            countType   = "imageCountMoreThan"
+            tagStatus   = "any"
+          }
+        },
+      ]
+    }
+  )
+  repository = each.value.name
+}
